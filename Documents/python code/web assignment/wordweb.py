e43256caf519af3afbfed07e5f7bd310c7e61903
Darran Gahan <d.m.gahan@gmail.com>
2017-11-12 21:13:44 +0000

    Source word outputs to running page

--- Documents/python code/web assignment/wordweb.py
+++ Documents/python code/web assignment/wordweb.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+import word_utils
 
 app = Flask(__name__)
 
@@ -12,11 +13,23 @@ def input():
     return render_template('input.html',
                            the_title = 'Input page.')
 
+
+@app.route('/running')
+def running():
+    word_utils.pre_process_words()
+    sourceword = word_utils.get_source_word()
+    session['sourceword'] = sourceword
+    return render_template('running.html',
+                           the_title = 'WordGame',
+                           the_word = sourceword)
+
 @app.route('/winner')
 def winner():
+    source = word_utils.get_source_word()
     return render_template('winner.html',
                            the_tilte = 'Winner.')
 
 
 if __name__ == '__main__':
+    app.secret_key = "adfs;45;']sgakltuuos;ga]][g"
     app.run(debug=True)
