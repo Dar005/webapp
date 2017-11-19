fe38d80531b00beda57af140df2e565faa61fe2d
Darran Gahan <d.m.gahan@gmail.com>
2017-11-19 19:35:20 +0000

    functioning

--- Documents/python code/web assignment/wordweb.py
+++ Documents/python code/web assignment/wordweb.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html',
-                           the_title = 'WordGame web.')
+                           the_title = 'WordGame.')
 
 
 @app.route('/running')
@@ -23,44 +23,48 @@ def running():
 
     return render_template('running.html',
                            the_title = 'WordGame',
-                           the_word = sourceword)
+                           sourceword = sourceword)
 
 
 @app.route('/processwords', methods=['GET', 'POST'])
 def word_process():
-    # Gets and return the user_input
-    if request.method == 'GET':
-        return render_template('running.html',
-                               the_title = 'error....')
-    elif request.method == 'POST':
+    # Gets and returns the user_input
+    if request.method == 'POST':
         session['end_time'] = time.perf_counter()
         user_input = request.form['user_input']
         session['user_input'] = user_input
-        winner, status, words = check_words(session.get('sourceword'), user_input)
+        winner, words = check_words(session.get('sourceword'), user_input)
         session['winner'] = winner
-        print(winner, status)
         if winner:
+            ## if user wins
             session['time_taken'] = session['end_time'] - session['start_time']
+            final_time = round(session['time_taken'], 2)
+            session['final_time'] = final_time
             return render_template('winner.html',
-                                   time_taken = session['time_taken'],)
+                                   time_taken = session['time_taken'],
+                                   final_time = final_time,
+                                   the_title = 'Winner')
 
         return render_template('loser.html',
+                               ## if user loses
                              the_tilte = 'user info',
-                             the_word = session.get('sourceword'),
+                             sourceword = session.get('sourceword'),
                              user_input = user_input,
-                             status = status,
-                             words = words)
+                             words = words,
+                             the_title = 'Wordgame')
+
 
 @app.route('/addwinner', methods=['GET','POST'])
 def add_winner():
     if request.method == 'POST':
         if session.get('winner'):
-            data_utils.add_to_scores(request.form.get('user_name'), session.get('time_taken'))
+            data_utils.add_to_scores(request.form.get('user_name'), session.get('final_time'))
             session['winner'] = False
             session['time_taken'] = False
             leaderboard = data_utils.get_sorted_leaderboard()[:10]
             return render_template('leaderboard.html',
-                                   leaderboard = leaderboard)
+                                   leaderboard = leaderboard,
+                                   the_title = 'Wordgame')
         return redirect('/')
 
 
